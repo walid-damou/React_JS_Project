a23ecd56883d9895e0c7c29b06098add1737d9d2
chaima-iz <shaymaeizwaghne.01@gmail.com>
2023-01-07 13:35:13 +0100

    tailwind added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './App.css';
 import Header from './layouts/Header'
 import Footer from './layouts/Footer'
 import Home from './pages/Home'
 function App() {
   return (
-    <div className="App">
+    <div className="BeVietnamPro m-0 p-0">
      <Header/>
      <Home/>
      <Footer/>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        BeVietnamPro: ["Be Vietnam Pro", "sans-serif"],
+      }
+    },
+  },
+  plugins: [],
+}
