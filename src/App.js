2b064a7914c86884855d67f14cd374366ef6e5a9
chaima-iz <shaymaeizwaghne.01@gmail.com>
2023-01-07 13:12:00 +0100

    file structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
 import React from 'react';
-
+import Header from './layouts/Header'
+import Footer from './layouts/Footer'
+import Home from './pages/Home'
 function App() {
   return (
     <div className="App">
-      <h2 > hello world !</h2>
+     <Header/>
+     <Home/>
+     <Footer/>
     </div>
   );
 }
