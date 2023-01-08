871fa540105c917fd56f4a7070ad13245773d791
walid damou <walid.damou.2015@gmail.com>
2023-01-08 16:36:35 +0100

    Restaurants API

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import './App.css';
 import Header from './layouts/Header'
 import Footer from './layouts/Footer'
 import Home from './pages/Home'
+import Restaurants from './components/Restaurants/Restaurants'
+import { BrowserRouter as Router ,Route ,Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="BeVietnamPro m-0 p-0">
-     <Header/>
-     <Home/>
-     <Footer/>
+     
+     <Router>
+        
+
+        <Routes>
+            <Route exact path='/' element={<><Header/><Home/><Footer/></>} />
+            <Route exact path='/Restaurants' element={<><Header/><Restaurants/><Footer/></>} />
+        </Routes>
+    
+    </Router>
     </div>
   );
 }
