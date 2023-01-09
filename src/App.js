c6a8deaf5b07ef2c93629fd99231118407dcf2a3
DraouiAicha <aicha.draoui-etu@etu.univh2c.ma>
2023-01-09 22:48:31 +0100

    weather_first_commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Restaurants from './components/Restaurants/Restaurants'
 import RestauDetails from './components/Restaurants/RestauDetails'
 import { BrowserRouter as Router ,Route ,Routes } from 'react-router-dom';
 import Hotels from './pages/Hotels';
+import Weather from './pages/Weather';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
      <Header/>
         <Routes>
             <Route exact path='/' element={<Home/>} />
+            <Route exact path='/Weather' element={<Weather/>}></Route>
             <Route exact path='/Restaurants' element={<Restaurants/>} />
             <Route exact path='/Hotels' element={<Hotels/>} />
             <Route exact path='/Restaurants/Details/:id' element={<RestauDetails/>} />
